374943958cf323311144d18392f4d828c3e0ec81
illgum <ailham52@gmail.com>
2025-12-15 21:47:52 +0300

    Миграции

--- python/databases/orm_example/models.py
+++ python/databases/orm_example/models.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import List, Optional
 from sqlalchemy import ForeignKey, String
 from sqlalchemy.orm import (
@@ -13,6 +12,7 @@ class Base(DeclarativeBase):
 class Product(Base):
     __tablename__ = "products"
     name: Mapped[str] = mapped_column(String(255), nullable=False)
+    # description: Mapped[str] = mapped_column(String(255), nullable=False)
     price: Mapped[float] = mapped_column(nullable=False)
     quantity: Mapped[int] = mapped_column(nullable=False)
     category_id: Mapped[Optional[int]] = mapped_column(
