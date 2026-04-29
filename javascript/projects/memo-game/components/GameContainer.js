d81aa79500b28678945f09121db870bce1fc286e
illgum <ailham52@gmail.com>
2026-04-29 21:49:53 +0300

    Игра 'Найди пару' - итог

--- javascript/projects/memo-game/components/GameContainer.js
+++ javascript/projects/memo-game/components/GameContainer.js
@@ -9,26 +9,55 @@ export class GameContainer {
     }
     this._rows = rows;
     this._cols = cols;
-    this._cardElements = this.generateCards();
+    this._openedCards = [];
+    this._maxRemainingTries = triesCount;
+    this._remainingTries = 0;
+    this._cardElements = [];
+
+    this._element.addEventListener("click", this.onCardClick.bind(this));
     this._triesCounter = document.createElement("p");
     this._triesCounter.className = "tries-counter";
-    this.updateTriesCounter(triesCount);
-    this._openedCards = [];
+    this._cardsListContainer = document.createElement("div");
+    this._cardsListContainer.className = "cards-container";
+    this._cardsList = document.createElement("ol");
+    this._cardsList.classList.add("cards");
+    this._gameOverElement = document.createElement("p");
+    this._gameOverElement.classList.add("game-over", "hidden");
+    this._gameOverElement.textContent = "Game Over";
+    this._restartButton = document.createElement("button");
+    this._restartButton.textContent = "Restart";
+    this._restartButton.classList.add("restart-button", "hidden");
+    this._restartButton.addEventListener("click", this.restart.bind(this));
+  }
+
+  appendDefaultChildren() {
+    this._element.appendChild(this._triesCounter);
+    this.appendCardsChildren();
+    this._cardsListContainer.appendChild(this._cardsList);
+    this._element.appendChild(this._cardsListContainer);
+    this._cardsListContainer.appendChild(this._gameOverElement);
+    this._cardsListContainer.appendChild(this._restartButton);
   }
 
   initialize() {
-    this._element.appendChild(this._triesCounter);
+    this._openedCards = [];
+    this._cardElements = this.generateCards();
+    this.updateTriesCounter(this._maxRemainingTries);
+  }
 
-    const cardsListContainer = document.createElement("div");
-    cardsListContainer.className = "cards-container";
-    const cardsList = document.createElement("ol");
-    cardsList.classList.add("cards");
+  restart() {
+    this._cardElements.forEach((card) => card.getElement().remove());
+    this.initialize();
+    this.appendCardsChildren();
+    this._gameOverElement.classList.add("hidden");
+    this._cardsList.classList.remove("hidden");
+    this._restartButton.classList.add("hidden");
+  }
+
+  appendCardsChildren() {
     this._cardElements.forEach((card) =>
-      cardsList.appendChild(card.getElement()),
+      this._cardsList.appendChild(card.getElement()),
     );
-    cardsListContainer.appendChild(cardsList);
-    this._element.appendChild(cardsListContainer);
-    this.addCardClickListener();
   }
 
   updateTriesCounter(remainingTries) {
@@ -36,48 +65,42 @@ export class GameContainer {
     this._triesCounter.textContent = this._remainingTries;
   }
 
-  addCardClickListener() {
-    this._element.addEventListener("click", (event) => {
-      const cardHTMLElement = event.target.closest(`.${CardElement.className}`);
-      if (!cardHTMLElement) {
-        return;
-      }
+  onCardClick(event) {
+    const cardHTMLElement = event.target.closest(`.${CardElement.className}`);
+    if (!cardHTMLElement) {
+      return;
+    }
 
-      const selectedCard = this._cardElements.find(
-        (cardElement) => cardElement.getElement() == cardHTMLElement,
-      );
+    const selectedCard = this._cardElements.find(
+      (cardElement) => cardElement.getElement() == cardHTMLElement,
+    );
 
-      if (selectedCard.guessed) return;
+    if (selectedCard.guessed) return;
 
-      if (this._openedCards.length >= this._maxOpenedCount) {
-        const isEqualToFirst = (card) => card.isEqual(this._openedCards[0]);
-        if (this._openedCards.every(isEqualToFirst)) {
-          this._openedCards.forEach((card) => (card.guessed = true));
-        } else {
-          this.updateTriesCounter(this._remainingTries - 1);
-          if (this._remainingTries <= 0) {
-            this._makeGameOver();
-          }
-          this._openedCards.forEach((card) => card.hide());
+    if (this._openedCards.length >= this._maxOpenedCount) {
+      const isEqualToFirst = (card) => card.isEqual(this._openedCards[0]);
+      if (this._openedCards.every(isEqualToFirst)) {
+        this._openedCards.forEach((card) => (card.guessed = true));
+      } else {
+        this.updateTriesCounter(this._remainingTries - 1);
+        if (this._remainingTries <= 0) {
+          this._makeGameOver();
         }
-        this._openedCards = [];
-      } else if (this._openedCards.at(-1) == selectedCard) {
-        return;
+        this._openedCards.forEach((card) => card.hide());
       }
+      this._openedCards = [];
+    } else if (this._openedCards.at(-1) == selectedCard) {
+      return;
+    }
 
-      this._openedCards.push(selectedCard);
-      selectedCard.show();
-    });
+    this._openedCards.push(selectedCard);
+    selectedCard.show();
   }
 
   _makeGameOver() {
-    const cardsList = this._element.querySelector(".cards");
-    const parent = cardsList.parentElement;
-    cardsList.remove();
-    const gameOverElement = document.createElement("p");
-    gameOverElement.className = "game-over";
-    gameOverElement.textContent = "Game Over";
-    parent.appendChild(gameOverElement);
+    this._cardsList.classList.add("hidden");
+    this._gameOverElement.classList.remove("hidden");
+    this._restartButton.classList.remove("hidden");
   }
 
   getElement() {
--- javascript/projects/memo-game/script.js
+++ javascript/projects/memo-game/script.js
@@ -5,3 +5,4 @@ import { GameContainer } from "./components/GameContainer.js";
 const gameElement = document.querySelector(".game");
 const game = new GameContainer(gameElement, 5, 4, 2);
 game.initialize();
+game.appendDefaultChildren();
